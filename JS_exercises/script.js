d603601a2d150bb6c061fa38a91a8a0e6370317a
Grady White <grady.w.white@gmail.com>
2016-06-13 19:54:02 -0700

    updated JS exercises (#2 and #3 still incomplete) and added EtchSketch folder

--- JS_exercises/script.js
+++ JS_exercises/script.js
@@ -1,12 +1,58 @@
+
+$(document).ready(function() {
+
+//problem #1
 var num = 0;
 
 for (i = 0; i < 1000; i++) {
-	if (i % 3 || 5 === 0) {
-		num += i;
-	} else {
-		null
-	};
-	};
+   if (i % 3 ===0 || i % 5 === 0) {
+       num += i;
+   };
 };
 
-console.log(num);
+   console.log(num);
+
+
+
+//problem #2
+var add = 1;
+var fib = 2;
+var sum = add + fib;
+var totalSum = 0;
+
+while (sum <= 4000000) {
+	if(sum % 2 === 0){
+	totalSum += sum;
+	}; 
+	add = fib;
+	fib = sum;
+	return totalSum;
+};
+
+
+
+//problem #3
+var numb = 24;
+var biggestPrime = 1;
+var multiples = [];
+
+for (i = 2; i < numb; i++){
+	if (numb % i === 0) {
+		multiples.push(i);
+		var isPrime = true;
+		for (j = 2; j < i; j++) {
+			if (i % j === 0) {
+				var isPrime = false;
+			}
+		}
+		if (isPrime === true) {
+			biggestPrime = i;
+		}
+	}
+}
+
+console.log(multiples);
+console.log(biggestPrime);
+
+
+});
